42f083d42128dc504bcdcec99d923320af3b42aa
hyo-young shin <hyashin1998@gmail.com>
2021-12-01 03:03:23 +0900

    마피아 의사 기능 추가 mafiaVsDoc 함수 기능안함

--- Mafia/src/Game.java
+++ Mafia/src/Game.java
@@ -44,8 +44,8 @@ public class Game {
 		survivorNum = player.size(); // 게임을 시작한 현재 게임 플레이어 수. 즉, 생존자 수.
 		totalPlayer = player.size();
 		deadNum = 0;
-		dayTime = survivorNum * 5000; // 낮 시간 (생존자수*15초)
-		nightTime = survivorNum * 50000; // 밤 시간 (생존자수*15초)
+		dayTime = survivorNum * 10000; // 낮 시간 (생존자수*10초)
+		nightTime = survivorNum * 10000; // 밤 시간 (생존자수*10초)
 	}
 
 	public void start() throws IOException {
@@ -121,13 +121,15 @@ public class Game {
 					System.out.println(SV1+" Sv1 come");
 
 					if(playerID == SV1+1) {
+						mafiaVsDoc();//마피아 의사 능력 결과 반영
 						checkWin(); //승리조건 확인
 					}
 					writer = new PrintWriter(socket.getOutputStream(),true);
 					writer.println("/d");	// protocol
 					writer.flush();
+					
+					checkDie();// 매 낮마다 죽은 사람 있는지 확인.
 
-					checkDie();	// 매 낮마다 죽은 사람 있는지 확인.
 				}catch(IOException ie){
 					System.out.println(ie.getMessage());
 				}
@@ -202,6 +204,7 @@ public class Game {
 					writer.flush();
 					
 					checkDie();
+					
 				}catch(IOException ie){
 					System.out.println(ie.getMessage());
 				}
@@ -511,8 +514,79 @@ public class Game {
 	//***********vote 관련 메소드 끝
 	
 	public void mafiaVsDoc() throws IOException{//마피아 의사 능력결과
+		File file1 = new File("mafiavoteInfo.txt");
+		File file2 = new File("docvoteInfo.txt");
+		BufferedReader brv1 = new BufferedReader(new FileReader(file1));
+		BufferedReader brv2 = new BufferedReader(new FileReader(file2));
+		int mafiaKilled;
+		String str1 ="";
+		String str2 ="";
+		String count = "";
+		
+		while((count = brv1.readLine()) != null) {
+			str1= brv1.readLine();
+		}
+		
+		while((count = brv2.readLine()) != null) {
+			str2= brv1.readLine();
+		}
+		
+		if(str1.equals(str2)&& (file1.length()!= 0) && (file2.length()!= 0) ) {
+			et.broadcast("의느님이 플레이어 "+ str1+ " 님을 살렸습니다!" );
+		}
+		else if((str1.equals(str2) == false)&& (file1.length()!= 0)){
+			 et.broadcast("마피아가 "+ str1+ " 님을 살해했습니다!" );
+			mafiaKilled = Integer.parseInt(str1);
+			mafiaResult(mafiaKilled);
+		}
+		 brv1.close();
+		 brv2.close();
+		
 		
 	}
+	
+	public void mafiaResult(int mafiKilled)throws IOException{//마피아 능력 결과 반영
+		int [][] array = new int [totalPlayer][4];
+		String str[] = null;
+		String s;
+
+
+		File file = new File("clientInfo.txt");
+
+		int k = 0;
+		str = new String[10];
+		BufferedReader brv = new BufferedReader(new FileReader(file));//파일 내용 전체 저장
+		while ((s = brv.readLine()) != null) {
+			str[k] = s;
+			k++;
+		}
+
+		for (int i = 0; i < totalPlayer; i++) {// 배열에 파일 정보 int 형태로 저장
+
+			s = str[i];
+			String split[] = s.split(" ");
+
+			array[i][0] = Integer.parseInt(split[0]);
+			array[i][1] = Integer.parseInt(split[1]);
+			array[i][2] = Integer.parseInt(split[2]);;
+			array[i][3] = Integer.parseInt(split[3]);
+		}
+		array[mafiKilled-1][2] = 1;//죽임처리
+
+		brv.close();
+
+		BufferedWriter bwv1 = new BufferedWriter(new FileWriter(file));// 파일 비우기
+		bwv1.close();
+
+		BufferedWriter bwv2 = new BufferedWriter(new FileWriter(file,true));
+		for (int i = 0; i < totalPlayer; i++) {
+			bwv2.write(array[i][0] + " " + array[i][1] +" " + array[i][2]+" " + array[i][3]);
+			bwv2.newLine();
+		}
+		bwv2.close();
+
+	}
+	
 
 	public void rand(int roles[], int playernum) {// 역할 배열 랜덤 섞기
 		Random rd = new Random();
@@ -658,19 +732,19 @@ public class Game {
 
 			if(mafiaNum == 0)
 			{
-				//writer = new PrintWriter(socket.getOutputStream(),true);
-				//writer.println("victory citizen");
-				//writer.flush();
-				et.broadcast("victory citizen");
+				writer = new PrintWriter(socket.getOutputStream(),true);
+				writer.println("victory citizen");
+				writer.flush();
+				//et.broadcast("victory citizen");
 
 				System.exit(0);
 			}
 			else if(mafiaNum >= citizenNum)
 			{
-				et.broadcast("victory mafia");
-				//writer = new PrintWriter(socket.getOutputStream(),true);
-				//writer.println("victory mafia");
-				//writer.flush();
+				//et.broadcast("victory mafia");
+				writer = new PrintWriter(socket.getOutputStream(),true);
+				writer.println("victory mafia");
+				writer.flush();
 
 				System.exit(0);
 			}
